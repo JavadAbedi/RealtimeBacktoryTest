93e9426366cbebdb7d32c256deeed686c3e8a9c0
jabedi <j.abedi70@gmail.com>
2016-05-30 20:58:06 +0430

    First commit

---
+++ Assets/CanvasController.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class CanvasController : MonoBehaviour {
+	public static string AUTHENTICATION_ID = "5738186ae4b0179857153b63";
+	public static string AUTHENTICATION_KEY = "5738186ae4b09a52993fe413";
+	public static string CONNECTIVITY_ID = "573818b7e4b0179857153b72";
+	public Button bConnect, bMatchMaking, bDisconnect, bClear, bGameMessage, bChat;
+	public Text tConsole;
+	public InputField etUsername, etPassword, etMessage;
+	string access_token = null;
+	ConnectivityServerHandler serverHandler = new ConnectivityServerHandler(CONNECTIVITY_ID);
+	ConnectToRealtimeServer connectToRealtimeServer;
+	Authentication authentication = (new Authentication(AUTHENTICATION_ID, AUTHENTICATION_KEY));
+	
+	// Use this for initialization
+	void Start () {
+		bConnect.onClick.AddListener(() => {
+			addToConsole("connecting ...");
+			authentication.login(etUsername.text, etPassword.text, (Login login) => {
+				if (login.access_token != null) {
+					addToConsole("connected");
+					access_token = login.access_token;
+					serverHandler.InitWebSocket(access_token);
+				}
+			});
+		});
+		
+		bClear.onClick.AddListener(() => {
+			tConsole.text = "";
+		});
+		
+		bDisconnect.onClick.AddListener(() => {
+			serverHandler.disconnect();
+		});
+		
+		bMatchMaking.onClick.AddListener(() => {
+			serverHandler.matchmakingRequest("matchmaking1", 55);
+		});
+		
+		bGameMessage.onClick.AddListener(() => {
+			connectToRealtimeServer.sendToChallenge(etMessage.text);
+		});
+		
+		bChat.onClick.AddListener(() => {
+			connectToRealtimeServer.sendToChallengeChat(etMessage.text);
+		});
+				
+		addListenerToServerHandler();
+	}
+	
+	private void addListenerToServerHandler() {
+		serverHandler.OnMatchFound += onMatchFound;
+		serverHandler.OnMatchResponse += onMatchResponse;
+		serverHandler.OnMatchCancellationResponse += onMatchCancellationResponse;
+		serverHandler.OnOpen += onOpen;
+		serverHandler.OnMessage += onMessage;
+		serverHandler.OnException += onException;
+		serverHandler.OnError += onError;
+		serverHandler.OnClose += onClose;
+		serverHandler.OnMatchNotFound += onMatchNotFound;
+		serverHandler.OnMatchUpdate += onMatchUpdate;
+		serverHandler.OnChatMessage += onChatMessage;
+		serverHandler.OnPushMessage += onPushMessage;
+	}
+	
+	public void onMatchFound(MatchFoundMessage matchFoundMessage) {
+		addToConsole("---Matchmaking with id " +
+                        matchFoundMessage.requestId +
+                        " and challenge id " +
+                        matchFoundMessage.realtimeChallengeId + " found");
+		connectToRealtimeServer = new ConnectToRealtimeServer(matchFoundMessage.address,
+			CONNECTIVITY_ID, access_token, matchFoundMessage.realtimeChallengeId, tConsole);
+		connectToRealtimeServer.init();
+	}
+
+	public void onMatchResponse(String requestId) {
+		addToConsole("---Your matchmaking request received with id: " + requestId);
+	}
+	
+	public void onMatchCancellationResponse(String requestId) {
+		addToConsole("---Your matchmaking request cancel with id: " + requestId);
+	}
+	
+	public void onOpen() {
+		addToConsole("---Connected");
+	}
+
+	public void onMessage(String message) {
+		addToConsole("---Message: " + message);
+	}
+
+	public void onException(String exceptionsString) {
+		addToConsole("---Exceptions: " + exceptionsString);
+	}
+	
+	public void onError(String message) {
+		addToConsole("---Error");
+	}
+
+	public void onClose() {
+		addToConsole("---Closed");
+	}
+
+	public void onMatchNotFound(MatchNotFoundMessage matchNotFoundMessage) {
+		addToConsole("---Matchmaking with id " +
+				matchNotFoundMessage.requestId + " not found");
+	}
+	
+	public void onMatchUpdate(MatchUpdateMessage matchUpdateMessage) {
+		addToConsole("---Matchmaking with id "
+				+ matchUpdateMessage.requestId + " updated");
+	}
+	
+	public void onChatMessage(ChatMessage chatMessage) {
+		addToConsole("---Chat message: " + chatMessage.message + " from: " + chatMessage.senderId
+				+ " at: " + chatMessage.date);
+	}
+
+	public void onPushMessage(PushNotifMessage pushNotifMessage) {
+		addToConsole("---Push message: " + pushNotifMessage.message);
+	}
+
+	// Update is called once per frame
+	void Update () {		
+	}
+	
+	void addToConsole(String str) {
+		tConsole.text = str + "\n" + tConsole.text;
+	}
+}
---
+++ Assets/ConnectToRealtimeServer.cs
@@ -0,0 +1,131 @@
+using System;
+using UnityEngine.UI;
+using System.Collections.Generic;
+public class ConnectToRealtimeServer {
+    String token = null;
+    String connectivityInstanceId = null;
+
+    public String realtimeChallengeId;
+    private String address;
+    public Text tConsole;
+	RealtimeServerHandler serverHandler;
+
+        public ConnectToRealtimeServer(String address, String connectivityInstanceId, 
+            String token, String realtimeChallengeId, Text tConsole) {
+        this.address = address;
+        this.realtimeChallengeId = realtimeChallengeId;
+        this.token = token;
+        this.connectivityInstanceId = connectivityInstanceId;
+        this.tConsole = tConsole;
+    }
+    
+    public void sendToChallenge(String message) {
+        serverHandler.sendToChallenge(message);
+    }
+    
+    public void sendToChallengeChat(string message) {
+        serverHandler.sendToChallengeChat(message);
+    }
+    
+    public void sendChallengeResult(List<String> winnersList) {
+        serverHandler.sendChallengeResult(winnersList);
+    }
+    
+    public void init() {
+        serverHandler = new RealtimeServerHandler(address, connectivityInstanceId, 
+            token, realtimeChallengeId);
+        serverHandler.InitWebSocket();
+        serverHandler.OnMessage += onMessage;
+        serverHandler.OnConnect += onConnect;
+        serverHandler.OnDisconnect += onDisconnect;
+        serverHandler.OnException += onException;
+        serverHandler.OnDirectChatMessage += onDirectChatMessage;
+        serverHandler.OnChallengeChatMessage += onChallengeChatMessage;
+        serverHandler.OnChallengeJoined += onChallengeJoined;
+        serverHandler.OnChallengeStarted += onChallengeStarted;
+        serverHandler.OnStartedWebhookMessage += onStartedWebhookMessage;
+        serverHandler.OnDirectChatMessage += onDirectChatMessage;
+        serverHandler.OnChallengeEventMessage += onChallengeEventMessage;
+        serverHandler.OnChallengeChatMessage += onChallengeChatMessage;
+        serverHandler.OnResultReceived += onResultReceived;
+        serverHandler.OnMasterMessage += onMasterMessage;
+        serverHandler.OnWebhookErrorMessage += onWebhookErrorMessage;
+        serverHandler.OnJoinedWebhookMessage += onJoinedWebhookMessage;
+        serverHandler.OnStartedWebhookMessage += onStartedWebhookMessage;
+        serverHandler.OnChallengeEndedMessage += onChallengeEndedMessage;
+        serverHandler.OnChallengeDisconnectMessage += onChallengeDisconnectMessage;
+        serverHandler.OnChallengeLeavedMessage += onChallengeLeavedMessage;
+    }
+    
+    public void onMessage(String message) {
+        addToConsole("---Message: " + message);
+    }
+    
+    public void onConnect() {
+        addToConsole("---Connected");
+    }
+
+    public void onDisconnect() {
+        addToConsole("---Disconnected");
+    }
+
+    public void onException(String exceptionsString) {
+        addToConsole("---Exceptions: " + exceptionsString);
+    }
+
+    public void onChallengeJoined(ChallengeJoinedMessage joinedMessage) {
+        addToConsole("---user joined: " + joinedMessage.userId + " name: " + joinedMessage.username);
+    }
+    public void onChallengeStarted(ChallengeStartedMessage startedMessage) {
+        addToConsole("---challenge started at: " + startedMessage.startedDate);
+    }
+    public void onStartedWebhookMessage(StartedWebhookMessage startedMessage) {
+        addToConsole("---started webhook message: " + startedMessage.message);
+    }
+    public void onDirectChatMessage(DirectChatMessage directMessage) {
+        addToConsole("---Direct Chat Message: " + directMessage.message + " from: " + directMessage.userId);
+    }
+    public void onChallengeEventMessage(ChallengeEventMessage challengeEventMessage) {
+        
+            addToConsole("---Challenge Event: " + challengeEventMessage.message + " from: "
+                    + challengeEventMessage.userId);
+    }
+    public void onChallengeChatMessage(ChallengeChatMessage chatMessage) {
+        addToConsole("---Challenge Chat Message: " + chatMessage.message + " from: " + chatMessage.userId);
+    }
+    public void onResultReceived(BacktoryRealtimeMessage resultReceived) {
+        addToConsole("---Your Result Received. Thanks for reporting!!!");
+    }
+    public void onMasterMessage(MasterMessage masterMessage) {
+        addToConsole("---Message from game master: " + masterMessage.message);
+    }
+    public void onWebhookErrorMessage(WebhookErrorMessage errorMessage) {
+        addToConsole("---Webhook error with status: " + errorMessage.httpStatus);
+    }
+    public void onJoinedWebhookMessage(JoinedWebhookMessage webhookMessage) {
+        addToConsole("---Joined webhook message: " + webhookMessage.message);
+    }
+    public void onChallengeEndedMessage(ChallengeEndedMessage endedMessage) {
+        List<String> winnersList = endedMessage.winners;
+        String winners = "";
+        if (winnersList.Count > 0) {
+            winners += winnersList[0];
+        }
+        for (int i = 1; i < winnersList.Count; i++)
+            winners += ", " + winnersList[i];
+
+        addToConsole("---Challenge ended with winners: " + winners);
+    }
+    public void onChallengeDisconnectMessage(ChallengeDisconnectMessage dcMessage) {
+        addToConsole("---User with id " + dcMessage.userId + " disconnected");
+    }
+    public void onChallengeLeavedMessage(ChallengeLeavedMessage leavedMessage) {
+        addToConsole("---Challenge leaved by: " + leavedMessage.userId + ", " +
+                leavedMessage.username);
+    }
+
+    
+    void addToConsole(String str) {
+		tConsole.text = str + "\n" + tConsole.text;
+	}
+}
